14731d1e7c656f21c43b4b94191e665e804a0c7b
fjliang56 <86655295+fjliang56@users.noreply.github.com>
2021-09-26 00:05:14 +0800

    添加 onClick 事件

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -5,11 +5,12 @@ import './icon.scss'
 
 interface IconProps {
     name: string,
+    onClick: ()=>void
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props)=>{
     return (
-            <svg className="hzw-icon">
+            <svg className="hzw-icon" onClick={props.onClick}>
                 <use xlinkHref={`#${props.name}`} />
             </svg>
     )
--- lib/index.tsx
+++ lib/index.tsx
@@ -3,12 +3,14 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import Icon from './icon';
 
-
+const fn =()=>{
+    console.log('fn')
+}
 
 ReactDOM.render(<div>
-    <Icon name="wechat" />,
-    <Icon name="QQ" />,
-    <Icon name="taobao" />,
-    <Icon name="zfb" />,
-    <Icon name="bilibili" />
+    <Icon name="wechat" onClick={fn}/>,
+    <Icon name="QQ" onClick={fn}/>,
+    <Icon name="taobao" onClick={fn}/>,
+    <Icon name="zfb" onClick={fn}/>,
+    <Icon name="bilibili" onClick={fn}/>
 </div>,document.querySelector('#root'))
