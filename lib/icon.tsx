e2f28d828fbd07b0b6ebef407cb4fd1c395be7a5
fjliang56 <86655295+fjliang56@users.noreply.github.com>
2021-09-26 00:43:31 +0800

    支持传classname

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,16 +1,17 @@
 import * as React from 'react';
 import './importIcons'
 import './icon.scss'
+import classnames from './helpers/classnames'
 
 
-interface IconProps {
-    name: string,
-    onClick: React.MouseEventHandler<SVGElement>,
+interface IconProps extends React.SVGAttributes<SVGElement>{
+    name: string;
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props)=>{
+    const {className, ...restProps} = props;
     return (
-            <svg className="hzw-icon" onClick={props.onClick}>
+            <svg className={classnames('hzw-icon',className)} {...restProps}>
                 <use xlinkHref={`#${props.name}`} />
             </svg>
     )
